72bd5393d403dfb01bb6e81099d15da74e87b495
Carlos Eduardo Frare <cefrare@outlook.com>
2018-12-06 20:29:40 -0200

    MVC AUla4

---
+++ ViagensOnline.Repositorios.SqlServerTests/ViagensOnlineDbContextTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ViagensOnline.Repositorios.SqlServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ViagensOnline.Dominio;
+
+namespace ViagensOnline.Repositorios.SqlServer.Tests
+{
+    [TestClass()]
+    public class ViagensOnlineDbContextTests
+    {
+        private readonly ViagensOnlineDbContext db = new ViagensOnlineDbContext();
+
+        [TestMethod()]
+        public void InserirTest()
+        {
+            var destino = new Destino();
+
+            destino.Cidade = "Sao Paulo";
+            destino.Nome = "Carlos";
+            destino.NomeImagem = "paulista.png";
+            destino.Pais = "Brasil";
+
+            db.Destinos.Add(destino);
+
+            db.SaveChanges();
+        }
+
+
+
+    }
+}
