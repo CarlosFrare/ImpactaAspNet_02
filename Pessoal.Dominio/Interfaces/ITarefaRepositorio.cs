8089aa880106618e830daecb11b281d0e985d5aa
Carlos Eduardo Frare <cefrare@outlook.com>
2018-12-10 20:30:30 -0200

    Projeto ToDo- Agendador de Tarefas

---
+++ Pessoal.Dominio/Interfaces/ITarefaRepositorio.cs
@@ -0,0 +1,20 @@
+﻿using Pessoal.Dominio.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// utilizacao Interfaces
+namespace Pessoal.Dominio.Interfaces
+{
+    public interface ITarefaRepositorio
+    {
+        int Inserir(Tarefa Tarefa);
+        Tarefa Selecionar(int id);
+        List <Tarefa> Selecionar();
+        void Atualizar(Tarefa Tarefa);
+        void Excluir(int id);
+
+    }
+}
---
+++ Pessoal.Repositorios.SqlServer/TarefaRepositorio.cs
@@ -0,0 +1,38 @@
+﻿using Pessoal.Dominio.Entidades;
+using Pessoal.Dominio.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pessoal.Repositorios.SqlServer
+{
+    public class TarefaRepositorio : ITarefaRepositorio
+    {
+        public void Atualizar(Tarefa Tarefa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Inserir(Tarefa Tarefa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Tarefa Selecionar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tarefa> Selecionar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
