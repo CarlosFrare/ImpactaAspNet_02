5a00fc5a3aa87551f1f65332bc91ef989b1fb47b
Carlos Eduardo Frare <cefrare@outlook.com>
2018-12-10 22:34:11 -0200

    Continuacao do projeto ToDO Pesssoal

--- Pessoal.Repositorios.SqlServer/TarefaRepositorio.cs
+++ Pessoal.Repositorios.SqlServer/TarefaRepositorio.cs
@@ -2,6 +2,9 @@
 using Pessoal.Dominio.Interfaces;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,9 +13,26 @@ namespace Pessoal.Repositorios.SqlServer
 {
     public class TarefaRepositorio : ITarefaRepositorio
     {
+        private readonly string 
+        stringConexao = ConfigurationManager.ConnectionStrings["pessoalSqlServer"].ConnectionString;
+
+
         public void Atualizar(Tarefa Tarefa)
         {
-            throw new NotImplementedException();
+
+            using (var conexao = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = new SqlCommand("TarefaAtualizar", conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddRange(Mapear(Tarefa).ToArray());
+                    comando.ExecuteNonQuery();
+                }
+
+            }
+
         }
 
         public void Excluir(int id)
@@ -22,7 +42,35 @@ namespace Pessoal.Repositorios.SqlServer
 
         public int Inserir(Tarefa Tarefa)
         {
-            throw new NotImplementedException();
+            using (var conexao  = new SqlConnection(stringConexao))
+            {
+                conexao.Open();
+
+                using (var comando = new SqlCommand("TarefaInserir",  conexao))
+                {
+                    comando.CommandType = CommandType.StoredProcedure;
+                    comando.Parameters.AddRange(Mapear(Tarefa).ToArray());
+                    return (int)comando.ExecuteScalar();
+                }
+            }
+        }
+
+        private List<SqlParameter> Mapear(Tarefa tarefa)
+        {
+            var parametros = new List<SqlParameter>();
+
+            if (tarefa.Id > 0)
+            {
+                parametros.Add(new SqlParameter("@Id", tarefa.Id));
+            }
+
+            parametros.Add(new SqlParameter("@Concluida", tarefa.Concluida));
+            parametros.Add(new SqlParameter("@Nome", tarefa.Nome));
+            parametros.Add(new SqlParameter("@Observacoes", tarefa.Observacoes));
+            parametros.Add(new SqlParameter("@Prioridade", tarefa.Prioridade));
+
+            return parametros;
+            //throw new NotImplementedException();
         }
 
         public Tarefa Selecionar(int id)
